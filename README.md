90aa7b97c67f1a98d94a4653ccc0f23925d31767
Dmytro Berezovskyi <berezovskyidmytro27@gmail.com>
2025-11-09 20:33:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# My Portfolio Project: Area Landing Page
+
+**Description:**
+Landing page for a conceptual product "Area". Fully responsive, animated, with smooth scroll and interactive element.
+
+**Technologies:**
+![HTML](https://img.icons8.com/?size=100&id=20909&format=png&color=000000)
+![CSS](https://img.icons8.com/?size=100&id=21278&format=png&color=000000) 
+![JS](https://img.icons8.com/?size=100&id=108784&format=png&color=000000)
+
+**Purpose:**
+Show responsive layout, working with animations and interactive components
+
+**Installation:**
+```bash
+git clone <repo>
+cd project-folder
+npm install
+npm start
---
+++ src/js/main.js
@@ -0,0 +1,60 @@
+const nav = document.querySelector('.nav');
+const menu = document.querySelector('#menu');
+const burgerBtn = document.querySelector('.burger');
+const menuItems = document.querySelectorAll('.menu__item');
+
+burgerBtn.addEventListener('click', function(event){
+    event.stopPropagation();
+    nav.classList.toggle('active');
+    burgerBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+})
+menu.addEventListener('click', (event) => {
+    const link = event.target.closest('a');
+    if (link) {
+        nav.classList.remove('active');
+        burgerBtn.classList.remove('active');
+        menu.classList.remove('active');
+    }
+})
+document.addEventListener('click', (event) => {
+    if (menu.classList.contains('active') && !menu.contains(event.target)) {
+        nav.classList.remove('active');
+        burgerBtn.classList.remove('active');
+        menu.classList.remove('active');
+    }
+})
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY >= 20) {
+        menu.classList.add('scrolled');
+    } else {
+        menu.classList.remove('scrolled');
+    }
+})
+
+const animatedElements = document.querySelectorAll('.animate-on-scroll');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('visible');
+        } else {
+            entry.target.classList.remove('visible');
+        }
+    })
+})
+
+animatedElements.forEach(el => observer.observe(el));
+
+function updateClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+
+    const currentTimeString = `${hours}:${minutes}`;
+    document.querySelector('.current-time').textContent = currentTimeString;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
